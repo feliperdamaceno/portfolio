ae2595daf440f691c913f390d69300ccea06cfc3
feliperdamaceno <feliperdamaceno@sent.com>
2024-04-11 21:06:46 +0100

    :european_castle: initial commit

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const SOCIAL_LINKS = [
+  {
+    href: 'https://www.linkedin.com/in/feliperdamaceno',
+    name: 'Linkedin'
+  },
+  {
+    href: 'https://github.com/feliperdamaceno',
+    name: 'GitHub'
+  },
+  {
+    href: 'mailto:feliperdamaceno@sent.com',
+    name: 'Email'
+  }
+]
