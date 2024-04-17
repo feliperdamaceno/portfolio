081d981b1ddc8610775e66515d02c65698c7b5df
feliperdamaceno <feliperdamaceno@sent.com>
2024-04-17 22:00:31 +0100

    feat: implement projects section

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,2 @@
-export const SOCIAL_LINKS = [
-  {
-    href: 'https://www.linkedin.com/in/feliperdamaceno',
-    name: 'Linkedin'
-  },
-  {
-    href: 'https://github.com/feliperdamaceno',
-    name: 'GitHub'
-  },
-  {
-    href: 'mailto:feliperdamaceno@sent.com',
-    name: 'Email'
-  }
-]
+export const FEATURED_REPOSITORIES =
+  /harmonize.sass|crypto-tracker|tide|cookbook-app|node-authorization-api/
